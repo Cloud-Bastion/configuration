10e3dfb934e6865c2c98fd88d29daf8207d4ba23
PXAV <43720275+PXAV@users.noreply.github.com>
2024-09-16 16:26:27 +0200

    Implement KClassSearcher - change KClassFilter to be a functional interface extending from java Predicate<T>

--- src/main/kotlin/cloud/bastion/configuration/searcher/kclass/KClassFilter.kt
+++ src/main/kotlin/cloud/bastion/configuration/searcher/kclass/KClassFilter.kt
@@ -1,4 +1,16 @@
 package cloud.bastion.configuration.searcher.kclass
 
-class KClassFilter {
+import java.util.function.Predicate
+import kotlin.reflect.KClass
+
+fun interface KClassFilter: Predicate<KClass<*>> {
+    companion object {
+        fun annotatedWith(annotationClass: KClass<out Annotation>): KClassFilter {
+            return KClassFilter {
+                type -> type.annotations.any {
+                    it.annotationClass == annotationClass
+                }
+            }
+        }
+    }
 }
--- src/main/kotlin/cloud/bastion/configuration/searcher/kclass/KClassSearcher.kt
+++ src/main/kotlin/cloud/bastion/configuration/searcher/kclass/KClassSearcher.kt
@@ -1,4 +1,36 @@
 package cloud.bastion.configuration.searcher.kclass
 
+import com.google.common.base.Preconditions
+import com.google.common.reflect.ClassPath
+import com.google.inject.Inject
+import com.google.inject.Injector
+import com.google.inject.Singleton
+import com.google.inject.name.Named
+import kotlin.reflect.KClass
+
+@Singleton
 class KClassSearcher {
+
+    @Inject
+    private lateinit var injector: Injector
+
+    @Inject
+    @Named("SearcherClassLoader")
+    private lateinit var classLoader: ClassLoader
+
+    fun filter(packageNames: Array<String>, vararg typeFilters: KClassFilter): Sequence<KClass<*>> {
+        Preconditions.checkNotNull(packageNames)
+        println("TRYING TO RUN TEST with classloader: ${classLoader.name}")
+        return try {
+            val classPath = ClassPath.from(classLoader)
+            (if (packageNames.isEmpty()) classPath.allClasses
+            else packageNames.asSequence().flatMap { name -> classPath.getTopLevelClassesRecursive(name).asSequence() }
+                .toList()).asSequence().mapNotNull { classInfo -> classInfo.load().kotlin }
+                .filter { type -> typeFilters.all { it.test(type) } }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emptySequence()
+        }
+    }
+
 }
