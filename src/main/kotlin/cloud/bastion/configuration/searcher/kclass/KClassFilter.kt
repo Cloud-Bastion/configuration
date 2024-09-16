4a77bbf6d336ec789dc313dfa3e2b8c9f9afc6f2
PXAV <43720275+PXAV@users.noreply.github.com>
2024-09-16 22:40:43 +0200

    Add more filters to `KClassFilter`

--- src/main/kotlin/cloud/bastion/configuration/searcher/kclass/KClassFilter.kt
+++ src/main/kotlin/cloud/bastion/configuration/searcher/kclass/KClassFilter.kt
@@ -1,10 +1,16 @@
 package cloud.bastion.configuration.searcher.kclass
 
+import cloud.bastion.configuration.searcher.function.FunctionFilter
 import java.util.function.Predicate
 import kotlin.reflect.KClass
 
 fun interface KClassFilter: Predicate<KClass<*>> {
     companion object {
+        fun subClassOf(vararg classes: KClass<*>) {
+        }
+
+        fun matchFunctions(vararg functionFilter: FunctionFilter) {}
+
         fun annotatedWith(annotationClass: KClass<out Annotation>): KClassFilter {
             return KClassFilter {
                 type -> type.annotations.any {
