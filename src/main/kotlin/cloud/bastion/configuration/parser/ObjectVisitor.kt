3f117b7274a5af27981312e5505da3be0919e1dc
PXAV <43720275+PXAV@users.noreply.github.com>
2024-09-20 15:41:48 +0200

    Replace `SimplePropertyVisitor.kt` with `ObjectVisitor` as every configuration file is treated as an object

---
+++ src/main/kotlin/cloud/bastion/configuration/parser/ObjectVisitor.kt
@@ -0,0 +1,108 @@
+package cloud.bastion.configuration.parser
+
+import cloud.bastion.configuration.parsers.generated.BastionYMLBaseVisitor
+import cloud.bastion.configuration.parsers.generated.BastionYMLParser
+import com.google.inject.Inject
+import com.google.inject.Injector
+import org.antlr.v4.kotlinruntime.tree.ParseTree
+import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.full.memberProperties
+
+class ObjectVisitor: BastionYMLBaseVisitor<Any?>() {
+
+    @Inject
+    private lateinit var injector: Injector
+
+    private var objectClass: KClass<*>? = null
+
+    fun BastionYMLParser.ValueContext.isPrimitive(): Boolean {
+        return !(this is BastionYMLParser.ListValueContext
+                || this is BastionYMLParser.MapValueContext
+                || this is BastionYMLParser.ObjectValueContext)
+    }
+
+    fun <Receiver> KMutableProperty.Setter<Receiver>.callSafe(receiver: Any?, value: BastionYMLParser.ValueContext) {
+        when (value) {
+            is BastionYMLParser.StringValueContext -> {
+                this.call(receiver, value.text)
+            }
+
+            is BastionYMLParser.IntegerValueContext -> {
+                this.call(receiver, value.text.toInt())
+            }
+
+            is BastionYMLParser.BooleanValueContext -> {
+                this.call(receiver, value.text.toBoolean())
+            }
+
+            is BastionYMLParser.FloatValueContext -> {
+                this.call(receiver, value.text.toFloat())
+            }
+
+            else -> {
+                TODO("Throw exception, unhandled data type")
+            }
+        }
+    }
+
+    fun visit(tree: ParseTree, objectClass: KClass<*>): Any? {
+        this.objectClass = objectClass
+        return super.visit(tree)
+    }
+
+    override fun visitProperty(ctx: BastionYMLParser.PropertyContext): Any? {
+        checkNotNull(this.objectClass)
+
+        ctx.ID().forEachIndexed { index, keyContext ->
+            println("")
+            val key: String = keyContext.text
+            val value: BastionYMLParser.ValueContext = ctx.value(index)
+                ?: TODO("Throw exception that value is not set properly in config file")
+
+            if (value.isPrimitive()) {
+                val instance = injector.getInstance(this.objectClass!!.java)
+                val changeProperty = objectClass!!.memberProperties.find { it.name == key }
+                if (changeProperty is KMutableProperty<*>) {
+                    changeProperty.setter.callSafe(instance, value)
+                    println("Set value of $key to ${value.text}")
+                    // changeProperty.setter.call(instance, value.text)
+                } else {
+                    TODO("Exception: property does not exist or is declared as immutable (val). Try changing it to var.")
+                }
+            }
+
+//            if (value is BastionYMLParser.ListValueContext) {
+//                println("Property contains a list value!")
+//
+//
+//                val instance = injector.getInstance(this.objectClass!!.java)
+//                val changeProperty = objectClass!!.memberProperties.find { it.name == key }
+//                if (changeProperty is KMutableProperty<*>) {
+//                    changeProperty.setter.call(instance, ListVisitor().visitList(value.list()))
+//                    println("Set value of $key to ${value.text}")
+//                    // changeProperty.setter.call(instance, value.text)
+//                } else {
+//                    TODO("Exception: property does not exist or is declared as immutable (val). Try changing it to var.")
+//                }
+//            }
+
+
+        }
+        return super.visitProperty(ctx)
+    }
+
+    override fun visitList(ctx: BastionYMLParser.ListContext): Any? {
+        return super.visitList(ctx)
+    }
+
+    override fun visitObject(ctx: BastionYMLParser.ObjectContext): Any? {
+
+
+        return super.visitObject(ctx)
+    }
+
+    override fun defaultResult(): Any? {
+        return null
+    }
+}
