7a8902337d84132c01b7aec998854365ceec4e5a
PXAV <43720275+PXAV@users.noreply.github.com>
2024-09-20 15:42:31 +0200

    Implement `ListVisitor` allowing to extract infinitely nested lists from a configuration

---
+++ src/main/kotlin/cloud/bastion/configuration/parser/ListVisitor.kt
@@ -0,0 +1,23 @@
+package cloud.bastion.configuration.parser
+
+import cloud.bastion.configuration.parsers.generated.BastionYMLBaseVisitor
+import cloud.bastion.configuration.parsers.generated.BastionYMLParser
+
+class ListVisitor: BastionYMLBaseVisitor<List<Any>>() {
+
+    override fun visitList(ctx: BastionYMLParser.ListContext): List<Any> {
+        val output: MutableList<Any> = mutableListOf()
+        ctx.value().forEach {
+            if (it is BastionYMLParser.ListValueContext) {
+                visitList(it.list())
+            }
+            output.add(it.text)
+        }
+        return output
+    }
+
+    override fun defaultResult(): List<Any> {
+        return emptyList<Any>()
+    }
+
+}
